b8f90d3aef6a38fd8d8ea1fe0d527323d128edb9
David Peters <dwpeters2@gmail.com>
2012-07-31 11:51:25 -0500

    Fresh

---
+++ src/game/triggers/TriggerSystem.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.triggers;
+
+/**
+ *
+ * @author noctem
+ */
+public class TriggerSystem {
+    
+}
